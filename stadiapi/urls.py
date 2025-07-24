f690c2054a2c063dbf65031cd87a29e0155517e0
MBenman <maxwell.benman@gmail.com>
2025-07-24 00:25:40 -0400

    setup API

--- stadiapi/urls.py
+++ stadiapi/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..stadiapp.api import api
+from stadiapp.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- stadiapp/api.py
+++ stadiapp/api.py
@@ -1,4 +1,4 @@
-from ninja import NinjaAPI, 
+from ninja import NinjaAPI
 from .models import Stadium
 from .schemas import StadiumSchema, CreateStadiumSchema
 from django.shortcuts import get_object_or_404 
@@ -15,4 +15,22 @@ def create_stadium(request, payload: CreateStadiumSchema):
     stadium = Stadium.objects.create(**payload.dict())
     return stadium
 
+@api.get("stadiums/{stadium_id}", response=StadiumSchema)
+def get_stadium(request, stadium_id: int):
+    stadium = get_object_or_404(Stadium, id=stadium_id)
+    return stadium
+
+@api.put("stadiums/{stadium_id}", response=StadiumSchema)
+def update_stadium(request, stadium_id: int, payload: CreateStadiumSchema):
+    stadium = get_object_or_404(Stadium, id=stadium_id)
+    for attr, value in payload.dict().items():
+        setattr(stadium, attr, value)
+    stadium.save()
+    return stadium
+
+@api.delete("stadiums/{stadium_id}")
+def delete_stadium(request, stadium_id: int):
+    stadium = get_object_or_404(Stadium, id=stadium_id)
+    stadium.delete()
+    return {"success": True}
 
