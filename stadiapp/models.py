dec37d3bbd321ee528b9a9895321d5bdae96b0b0
MBenman <maxwell.benman@gmail.com>
2025-09-04 22:53:10 -0400

    Dockerize modifications

--- stadiapp/models.py
+++ stadiapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Stadium(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     sport = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
